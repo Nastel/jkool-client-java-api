a5c5e6f7e712841c9a2b9e4e8a29cd0db09bd231
Cathy Bernardone <cbernardone@nastel.com>
2015-07-06 16:01:18 -0400

    Bug fixes.

--- jKoolJavaRestClients/src/main/java/io/swagger/client/api/RunApi.java
+++ jKoolJavaRestClients/src/main/java/io/swagger/client/api/RunApi.java
@@ -14,23 +14,22 @@ public class RunApi {
 		{
 			EventActivity event = new EventActivity();
 			event.setCompCode("SUCCESS");
-			event.setCompCodeNo(0);
-			event.setElapsedTimeUsec(500);
+			//event.setCompCodeNo(0);
 			event.setIdCount("3");
-			event.setTrackingId("3175921a-1107-11e3-b8b0-600292390f03");
+			event.setTrackingId("3175921a-1107-11e3-b8b0-600292390f04");
 			event.setPid(5455); 
 			event.setTid(3);
 			event.setSourceFqn("APPL=WebOrders#SERVER=WebServer100#NETADDR=11.0.0.2#DATACENTER=DC1#GEOADDR=New York, NY");
 			event.setSourceInfo("Cathy3's source");
 			event.setSourceUrl("https://www.sample.com/orders/parts");
 			event.setSeverity("SUCCESS");
-			event.setSeverityNo(4);
+			//event.setSeverityNo(4);
 			event.setType("RECEIVE");
 			event.setTypeNo(4);
 			event.setReasonCode(0);
 			event.setLocation("New York, NY");
 			event.setOperation("ReceiveOrder");
-			event.setUser("Cathy3");
+			event.setUser("Cathy111");
 			String todaysDate = (new Date()).getTime() + "000";
 			event.setTimeUsec(todaysDate);
 			event.setStartTimeUsec(todaysDate);
@@ -43,14 +42,21 @@ public class RunApi {
 			event.setEncoding("none");
 			event.setResource("order/parts");
 			event.setMsgMimeType("text/plain");
-			event.setCorrId("OrderId:123@1434115730580806@1");
+			event.setCorrId("OrderId:123@1434115730580807@1");
+			event.setMsgTag("CathysMsg");
+			event.setException("CathysException");
+			event.setWaitTimeUsec("CathysWait");
+			event.setMsgAge(9999);
+			event.setStatus("CathysStatus");
+			event.setSource("CathysSource");
 			
 			Map<String, String> queryParams = new HashMap<String, String>();
 			Map<String, String> headerParams = new HashMap<String, String>();
 			Map<String, String> formParams = new HashMap<String, String>();
 			
 			//headerParams.put("token", "abc12345");
-			headerParams.put("token", "SqGGT2gMfDnifN65nwVXJUi0emJpNmti");
+			//headerParams.put("token", "SqGGT2gMfDnifN65nwVXJUi0emJpNmti");
+			headerParams.put("token", "2xLE44s5NICfXhVqNhzrkRQrb46tyHhM");
 			
 			ApiClient apiClient = new ApiClient();
 			apiClient.invokeAPI(apiClient.getBasePath(), "POST", queryParams, event, headerParams, null, "application/json",  "application/json",  null);

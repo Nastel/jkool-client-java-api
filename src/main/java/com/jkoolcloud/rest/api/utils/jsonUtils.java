4511946479611261cf3977d64253265a25cc66cf
Albert <albertm@jkoolcloud.com>
2016-08-05 22:37:04 -0400

    naming conventions
    naming conventions

--- src/main/java/com/jkoolcloud/rest/api/utils/jsonUtils.java
+++ src/main/java/com/jkoolcloud/rest/api/utils/jsonUtils.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
-public class jsonUtils {
+public class JsonUtils {
 	public static ObjectMapper MAPPER;
 
 	static {

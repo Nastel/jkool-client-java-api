549281cf15e2626512cdd0ef2f5f609fb2f7ebce
AlbertM <amavashev@nastel.com>
2016-08-08 15:42:22 -0400

    skeleton for websocket async queries
    skeleton for websocket async queries

---
+++ src/main/java/com/jkoolcloud/rest/api/service/JKQueryAsync.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014-2015 JKOOL, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jkoolcloud.rest.api.service;
+
+import javax.ws.rs.core.Response;
+
+
+public class JKQueryAsync extends JKQuery {
+	
+	WebsocketClient socket;
+	
+	/**
+	 * Call query in async mode using a callback
+	 * 
+	 * @param query JKQL query
+	 * @return callback callback class
+	 * @throws JKApiException
+	 */
+	public String call(String query, JKResultCallback callback) throws JKApiException {
+		return null;
+	}
+	
+	public Response cancel(String subid) throws JKApiException {
+		return super.call("unsubscribe " + subid);
+	}
+}
---
+++ src/main/java/com/jkoolcloud/rest/api/service/WebsocketClient.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2014-2015 JKOOL, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jkoolcloud.rest.api.service;
+
+import java.net.URI;
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.ContainerProvider;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+/**
+ *
+ */
+@ClientEndpoint
+public class WebsocketClient {
+
+	Session userSession = null;
+	private MessageHandler messageHandler;
+
+	public WebsocketClient(URI uri) {
+		try {
+			WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+			container.connectToServer(this, uri);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Callback hook for Connection open events.
+	 *
+	 * @param userSession
+	 *            the userSession which is opened.
+	 */
+	@OnOpen
+	public void onOpen(Session userSession) {
+		System.out.println("opening websocket");
+		this.userSession = userSession;
+	}
+
+	/**
+	 * Callback hook for Connection close events.
+	 *
+	 * @param userSession
+	 *            the userSession which is getting closed.
+	 * @param reason
+	 *            the reason for connection close
+	 */
+	@OnClose
+	public void onClose(Session userSession, CloseReason reason) {
+		System.out.println("closing websocket");
+		this.userSession = null;
+	}
+
+	/**
+	 * Callback hook for Message Events. This method will be invoked when a client send a message.
+	 *
+	 * @param message
+	 *            The text message
+	 */
+	@OnMessage
+	public void onMessage(String message) {
+		if (this.messageHandler != null) {
+			this.messageHandler.handleMessage(message);
+		}
+	}
+
+	/**
+	 * register message handler
+	 *
+	 */
+	public void addMessageHandler(MessageHandler msgHandler) {
+		this.messageHandler = msgHandler;
+	}
+
+	/**
+	 * Send a message.
+	 *
+	 */
+	public void sendMessage(String message) {
+		this.userSession.getAsyncRemote().sendText(message);
+	}
+
+	/**
+	 * Message handler.
+	 *
+	 */
+	public static interface MessageHandler {
+		public void handleMessage(String message);
+	}
+}

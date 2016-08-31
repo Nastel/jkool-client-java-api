c74e4b0d0ea3aaa4e1d04dfd9ca22b4f3e39ab01
AlbertM <amavashev@nastel.com>
2016-08-31 14:55:33 -0400

    cleanup
    cleanup

--- src/main/java/com/jkoolcloud/rest/api/service/JKQueryConstants.java
+++ src/main/java/com/jkoolcloud/rest/api/service/JKQueryConstants.java
@@ -37,9 +37,9 @@ public interface JKQueryConstants {
 	static final String JK_SUB_UUID_PREFIX		= "$sub/";
 	static final String JK_SUB_QUERY_PREFIX		= "subscribe to ";
 	static final String JK_UNSUB_QUERY_PREFIX	= "unsubscribe ";	
+	static final String JK_SEARCH_QUERY_PREFIX 	= "get events where message contains \"%s\"";
 
 	static final int DEFAULT_MAX_ROWS	= 100;
 	static final int DEFAULT_WAIT_TIME	= 20000;
 	static final int DEFAULT_RETRY_TIME = 0;
-	static final String JK_SEARCH_QUERY_PREFIX = "get events where message contains \"%s\"";
 }

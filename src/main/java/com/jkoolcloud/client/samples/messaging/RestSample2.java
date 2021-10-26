9f4210e45e62af69143edcd3383d20412245f098
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2021-10-26 09:24:32 +0300

    Updated copyright headers.

--- src/main/java/com/jkoolcloud/client/samples/messaging/RestSample2.java
+++ src/main/java/com/jkoolcloud/client/samples/messaging/RestSample2.java
@@ -1,13 +1,11 @@
-package com.jkoolcloud.client.samples.messaging;
-
 /*
- * Copyright 2014-2019 JKOOL, LLC.
+ * Copyright 2014-2021 JKOOL, LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +13,8 @@ package com.jkoolcloud.client.samples.messaging;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.jkoolcloud.client.samples.messaging;
 
 import java.net.InetAddress;
 import java.util.Arrays;
